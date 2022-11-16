5974fa2175b33590624abd751b0801a7d2b21109
Forgottenit <forgottenit@hotmail.com>
2022-11-16 17:29:16 +0000

    Added function to pair stock_data with column headings

--- run.py
+++ run.py
@@ -117,6 +117,16 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    Function to return dictionary of headings and user input
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    print(headings)
+    dictionary = dict(zip(headings, data))
+    return dictionary
+
+
 def main():
     """
     Run all program functions
@@ -129,7 +139,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
